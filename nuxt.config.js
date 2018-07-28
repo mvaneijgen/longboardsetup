221e98f0519b013c0c3b638ce396d485eed948f8
Mitchel van Eijgen <mvaneijgen@gmail.com>
2018-07-28 16:09:18 +0200

    Base url fix?

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,19 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+      { hid: "description", name: "description", content: "Nuxt.js project" },
+      { property: "og:type", content: "article" },
+      { property: "og:title", content: "Wish list app | M van Eijgen" },
+      {
+        property: "og:description",
+        content:
+          "Op de één of andere manier vinden mensen het lastig om iets voor mij als cadeau te bedenken, terwijl ik denk dat het super makkelijk is..."
+      },
+      { property: "og:url", content: "https://mvaneijgen.nl/longboardsetup" },
+      {
+        property: "og:image",
+        content: "https://mvaneijgen.nl/longboardsetup/social.jpg"
+      }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
