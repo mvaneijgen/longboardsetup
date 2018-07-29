75a6d44230b56d4bd44d90a5c12c44effd999001
Mitchel van Eijgen <mvaneijgen@gmail.com>
2018-07-29 12:18:52 +0200

    Fixed base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
   process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
         router: {
-          base: "/vanzwartnaargroen/"
+          base: "/longboardsetup/"
         }
       }
     : {};
