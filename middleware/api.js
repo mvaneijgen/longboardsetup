47694cb39d06e709879d4706b806d201ee40d773
Sascha Beining <beining@saltation.de>
2018-08-04 19:48:46 +0200

    Remove assigning unnecessary variables in api middleware
    The api results are copied two times to rebuild them for internal use. This is
    unnecessary and should be prevented by using the Array.map() method.
    
    The logic was merely moved and the lines of code that were commented out were
    left untouched. Normally though committing lines of code that are commented out
    is bad form and should be avoided.

--- middleware/api.js
+++ middleware/api.js
@@ -7,69 +7,56 @@ const queries = [
   "per_page=18",
   "_embed",
 ];
-function postTypeFunction(items) {
-  let itemsResult = [];
 
-  items.forEach(item => {
-    let itemObj = {
-      id: item.id,
-      title: item.title.rendered,
-      slug: item.slug,
-      type: item.type,
-      view: "advanced",
-      image:
+/**
+ * Converts the input data from the api into an internal data structure
+ * @param {Object} item
+ * @returns {Object}
+ */
+function fromInputData(item) {
+  return {
+    id: item.id,
+    title: item.title.rendered,
+    slug: item.slug,
+    type: item.type,
+    view: "advanced",
+    image:
+      item._embedded["wp:featuredmedia"][0].media_details.sizes.full
+        .source_url,
+    images: {
+      thumbnail:
+        item._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail
+          .source_url,
+      medium:
+        item._embedded["wp:featuredmedia"][0].media_details.sizes.medium
+          .source_url,
+      // large:
+      //   item._embedded["wp:featuredmedia"][0].media_details.sizes.large
+      //     .source_url,
+      full:
         item._embedded["wp:featuredmedia"][0].media_details.sizes.full
           .source_url,
-      images: {
-        thumbnail:
-          item._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail
-            .source_url,
-        medium:
-          item._embedded["wp:featuredmedia"][0].media_details.sizes.medium
-            .source_url,
-        // large:
-        //   item._embedded["wp:featuredmedia"][0].media_details.sizes.large
-        //     .source_url,
-        full:
-          item._embedded["wp:featuredmedia"][0].media_details.sizes.full
-            .source_url,
-      },
-    };
-
-    itemsResult.push(itemObj);
-  });
-  return itemsResult;
+    },
+  };
 }
 
 export default function({ params, store }) {
   return axios
     .get(`/wp/v2/decks?${queries.join("&")}`)
     .then(response => {
-      const itemsAll = response.data;
-
-      let itemsResult = postTypeFunction(itemsAll);
-
-      store.commit("items/addDecks", itemsResult);
+      store.commit("items/addDecks", response.data.map(fromInputData));
 
       // Done ➡️ next post type
       return axios.get(`/wp/v2/trucks?${queries.join("&")}`);
     })
     .then(response => {
-      const itemsAll = response.data;
-
-      let itemsResult = postTypeFunction(itemsAll);
-
-      store.commit("items/addTrucks", itemsResult);
+      store.commit("items/addTrucks", response.data.map(fromInputData));
 
       // Done ➡️ next post type
       return axios.get(`/wp/v2/wheels?${queries.join("&")}`);
     })
     .then(response => {
-      const itemsAll = response.data;
-
-      let itemsResult = postTypeFunction(itemsAll);
-
-      store.commit("items/addWheels", itemsResult);
+      store.commit("items/addWheels", response.data.map(fromInputData));
     })
     .catch(function(error) {
       store.commit("error", error);
